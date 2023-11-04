8f7b68d98e001ccff476f57ebb8b56e837d0b5e8
djon198360 <le1907@mail.ru>
2023-11-04 11:07:13 +0300

    create README

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const { PORT = 3000, API_URL = 'http://localhost' } = process.env;
-
 const app = express();
 
 app.get('/', (request, response) => {
