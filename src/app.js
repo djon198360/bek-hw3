055f53c0b3a936f40c6f192fa86a5e7fa7f2b92b
djon198360 <le1907@mail.ru>
2023-11-04 10:43:19 +0300

    add dotenv and config

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const { PORT = 3000, API_URL = 'http://localhost' } = process.env;
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.status(200);
+    response.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Ссылка на сервер: ${API_URL}:${PORT}`);
+});
